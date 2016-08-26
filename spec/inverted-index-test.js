df646f978707858ee69e8cf316f9ce69589c3c83
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-08-26 14:34:12 +0100

    add test to check if search can handle an array as an argument

--- spec/inverted-index-test.js
+++ spec/inverted-index-test.js
@@ -35,17 +35,17 @@ describe ('Populate index', function () {
 
   it('should create a new index when a file has been read', function () {
     expect(index.indexes).not.toBe({});
-    expect(typeof index.indexes[booksFileName]).toBe("object");
+    expect(typeof index.indexes[booksFileName]).toBe('object');
     expect(index.indexes[booksFileName]).not.toBe({});
   });
 
   it('should map string keys to correct objects', function() {
-    expect(index.indexes[booksFileName]["alice"]).toEqual([0]);
-    expect(index.indexes[booksFileName]["a"]).toEqual([0,1]);
-    expect(index.indexes[booksFileName]["an"]).toEqual([1]);
+    expect(index.indexes[booksFileName]['alice']).toEqual([0]);
+    expect(index.indexes[booksFileName]['a']).toEqual([0,1]);
+    expect(index.indexes[booksFileName]['an']).toEqual([1]);
   });
 
-  it("Ensure previous index is not overwritten by a new json file", function(){
+  it('Ensure previous index is not overwritten by a new json file', function(){
     index.createIndex(pages);
     pageFileName = index.getFileName(pages);
     expect(index.indexes[booksFileName]).not.toEqual(index.indexes[pages]);
@@ -63,7 +63,11 @@ describe ('Search Index', function() {
     fileName = index.getFileName(books);
   });
 
-  it("should return an array containing indices of the correct object", function() {
-    expect(index.searchIndex("a")).toEqual([[0,1]]);
+  it('should return an array containing indices of the correct object', function() {
+    expect(index.searchIndex('a')).toEqual([[0,1]]);
+  });
+
+  it('should handle an array of search terms', function() {
+    expect(index.searchIndex(['a', 'an', ['alice', 'wonderland', ['hobbit', 'dwarf']]])).toEqual([[0, 1], [1], [0], [0], [1], [1]]);
   });
 });
