1193f429455f96fd8bdec1ee7d66eb54470adb7f
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-08-26 14:15:23 +0100

    add helper functions to invertedindex class

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,6 +1,7 @@
 /* jslint node: true */
 /* eslint linebreak-style: ["error", "windows"]*/
 'use strict';
+
 const fs = require('fs');
 
 module.exports = class InvertedIndex {
@@ -15,10 +16,10 @@ module.exports = class InvertedIndex {
   }
 
   createIndex(filePath) {
-    const fileName = getFileName(filePath);
-    let fileContent = fs.readFileSynce(filePath, 'utf8');
+    const fileName = this.getFileName(filePath);
+    const fileContent = fs.readFileSync(filePath, 'utf8');
 
-    if (fileContent.replae(/\s+/g, '').length === 0) {
+    if (fileContent.replace(/\s+/g, '').length === 0) {
       throw new Error('file has no content');
     }
 
@@ -27,6 +28,69 @@ module.exports = class InvertedIndex {
     }
 
     this.indexFileContent[fileName] = fileContent;
-    this.indexes[fileName] = parseFileContent(fileName);
+    this.indexes[fileName] = this.parseFileContent(fileName);
   }
-}
+
+  // helper functions
+  getFileName(path) {
+    const pathArray = path.split('/');
+    const arrayLength = pathArray.length;
+    return pathArray[arrayLength - 1];
+  }
+
+  parseFileContent(fileName) {
+    let fileIndex = {};
+    const objectCount = {};
+    const fileContent = this.indexFileContent[fileName];
+    const fileJson = JSON.parse(fileContent);
+    let counter = 0;
+
+    for (let arrayPos of fileJson) {
+      const title = this.tokenize(arrayPos.title);
+      const text = this.tokenize(arrayPos.text);
+      const allContent = title.concat(text);
+      const uniqueContent = this.uniqueValues(allContent);
+
+      objectCount[counter] = {};
+      objectCount[counter] = uniqueContent;
+      counter++;
+    }
+
+    fileIndex = this.createFileIndex(objectCount);
+
+    return fileIndex;
+  }
+
+  createFileIndex(term) {
+    const termKeys = Object.keys(term);
+    const keyLength = termKeys.length;
+    const index = {};
+
+    for (let i = 0; i < keyLength; i++) {
+      term[i].forEach((val) => {
+        if (!Object.hasOwnProperty.call(index, val)) {
+          index[val] = [];
+        }
+
+        index[val].push(i);
+      });
+    }
+
+    return index;
+  }
+
+  tokenize(terms) {
+    const termWithoutPunct = this.sanitize(terms);
+    const termArray = termWithoutPunct.split(/\s+/g);
+    return termArray;
+  }
+
+  sanitize(term) {
+    return term.replace(/[.,:]/g, '').toLowerCase();
+  }
+
+  uniqueValues(terms) {
+    return terms.filter((value, pos, array) => array.indexOf(value) === pos
+    );
+  }
+};
