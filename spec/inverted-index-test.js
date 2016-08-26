d5889dd5df66f0fbf0eb4f6b75bb8ce8f97f9e32
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-08-26 08:16:37 +0100

    write populate index test suite, add test for checking if a new index has been created when a new file has been read

--- spec/inverted-index-test.js
+++ spec/inverted-index-test.js
@@ -1,6 +1,7 @@
-/*jslint node: true */
+/* jslint node: true */
+/* eslint linebreak-style: ["error", "windows"] */
 'use strict';
-const fs = require('fs');
+let fs = require('fs');
 let invertedIndex = require('../src/inverted-index.js');
 
 const books = 'jasmine/books.json';
@@ -10,13 +11,30 @@ const emptyFile = 'jasmine/inv-file.json';
 /**
 * Test suite for testing book.json data
 */
-describe('Read book data', () => {
-  it('should not be empty', () => {
+describe ('Read book data', () => {
+  it ('should not be empty', () => {
     fs.readFile(books, 'utf8', (err, data) => {
       if (err) throw (err);
       const fileContent = data.replace(/\s+/g, '');
-      expect(fileContent.length).toBeGreaterThan(0);
-      done();
+      expect (fileContent.length).toBeGreaterThan(0);
+      done ();
     });
+  });
+});
+
+/**
+ * Populate index test suite
+ */
+describe ('Populate index', () => {
+  beforeEach (() => {
+    index = new invertedIndex.invertedIndex();
+    index.createIndex(books);
+    booksFileName = index.getFileName(books);
+  });
+
+  it('should create a new index when a file has been read', () => {
+    expect(index.indexes).not.toBe({});
+    expect(typeof index.indexes[booksFileName]).toBe("object");
+    expect(index.indexes[booksFileName]).not.toBe({});
   });
 });
