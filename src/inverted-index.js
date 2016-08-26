e0cbbb87de167f25748df2c098fc3d88b0a79080
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-08-26 13:51:57 +0100

    add createindex function to invertedindex class

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -13,4 +13,20 @@ module.exports = class InvertedIndex {
     this.lastSearchedFile = '';
     this.searchResult = [];
   }
+
+  createIndex(filePath) {
+    const fileName = getFileName(filePath);
+    let fileContent = fs.readFileSynce(filePath, 'utf8');
+
+    if (fileContent.replae(/\s+/g, '').length === 0) {
+      throw new Error('file has no content');
+    }
+
+    if (this.indexName.indexOf(fileName) < 0) {
+      this.indexName.push(fileName);
+    }
+
+    this.indexFileContent[fileName] = fileContent;
+    this.indexes[fileName] = parseFileContent(fileName);
+  }
 }
