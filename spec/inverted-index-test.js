7de2bc4a2d7eaf96dd75ceb0f5d3c54534b77521
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-08-26 13:21:02 +0100

    add test for checking if string keys are mapped correctly

--- spec/inverted-index-test.js
+++ spec/inverted-index-test.js
@@ -1,21 +1,22 @@
 /* jslint node: true */
 /* eslint linebreak-style: ["error", "windows"] */
-'use strict';
-let fs = require('fs');
-let invertedIndex = require('../src/inverted-index.js');
+// 'use strict';
 
-const books = 'jasmine/books.json';
-const pages = 'jasmine/pages.json';
-const emptyFile = 'jasmine/inv-file.json';
+var fs = require('fs');
+var InvertedIndex = require('../src/inverted-index.js');
+
+var books = 'jasmine/books.json';
+var pages = 'jasmine/pages.json';
+var emptyFile = 'jasmine/inv-file.json';
 
 /**
 * Test suite for testing book.json data
 */
-describe ('Read book data', () => {
-  it ('should not be empty', () => {
-    fs.readFile(books, 'utf8', (err, data) => {
+describe ('Read book data', function () {
+  it ('should not be empty', function () {
+    fs.readFile(books, 'utf8', function (err, data) {
       if (err) throw (err);
-      const fileContent = data.replace(/\s+/g, '');
+      var fileContent = data.replace(/\s+/g, '');
       expect (fileContent.length).toBeGreaterThan(0);
       done ();
     });
@@ -25,16 +26,22 @@ describe ('Read book data', () => {
 /**
  * Populate index test suite
  */
-describe ('Populate index', () => {
-  beforeEach (() => {
-    index = new invertedIndex.invertedIndex();
+describe ('Populate index', function () {
+  beforeEach (function () {
+    index = new InvertedIndex();
     index.createIndex(books);
     booksFileName = index.getFileName(books);
   });
 
-  it('should create a new index when a file has been read', () => {
+  it('should create a new index when a file has been read', function () {
     expect(index.indexes).not.toBe({});
     expect(typeof index.indexes[booksFileName]).toBe("object");
     expect(index.indexes[booksFileName]).not.toBe({});
+  });
+
+  it('should map string keys to correct objects', function() {
+    expect(index.indexes[booksFilename]["alice"]).toEqual([0]);
+    expect(index.indexes[booksFileName]["a"]).toEqual([0,1]);
+    expect(index.indexes[booksFileName]["an"]).toEqual([1]);
   });
 });
