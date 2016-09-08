4e7c8013c1075f3bd7e1b69a76ca4bb54c8b1181
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-09-08 12:34:51 +0100

    modifications to test changes in inverted index

--- test.js
+++ test.js
@@ -1,6 +1,16 @@
 var invert = require('./src/inverted-index.js');
 var app = new  invert();
 
-app.createIndex('jasmine/books.json');
-console.log(app.searchIndex([]));
-console.log(app.searchIndex(" "));
+app.createIndex('jasmine/empty-object.json');
+console.log(app.getIndex('empty-object.json'));
+console.log(Object.keys(JSON.parse("{}")));
+
+// console.log(app.searchIndex(" "));
+//
+// var empty = JSON.parse('[{"we":8}]');
+//
+// console.log(empty.length);
+// console.log(Object.keys(empty[0]));
+// console.log({w:2}.toString());
+//
+// console.log(JSON.parse("sgdbf"));
